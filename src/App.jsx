82cdfdb3632693df7ff94804352ef6248dccada2
kign17019999 <purintanirat@hotmail.com>
2024-02-13 23:48:52 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,62 @@ import './App.css'
 import WheelComponent from "react-wheel-of-prizes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [previousResults, setPreviousResults] = useState([]);
 
   const segments = [
-    "better luck next time",
-    "won 70",
-    "won 10",
-    "better luck next time",
-    "won 2",
-    "won uber pass"
+    "โอนไว10บาท",
+    "โอนไว50บาท",
+    "กอด10ครั้ง",
+    "น้ำแซ่บ1แก้ว",
+    "ต่อยฟรี1ที",
+    "กินฟรี1มื้อ"
   ];
   const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
   const onFinished = (winner) => {
     console.log(winner);
+    setPreviousResults([...previousResults, winner]);
+  };
+
+  const appStyle = {
+    textAlign: 'left',
+    backgroundColor: 'pink',
+    padding: '20px' // Add some padding to the app container
   };
 
   const wheelContainerStyle = {
-    maxWidth: '80%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
+    border: '2px solid black', // Add a black border around the wheel container
+    borderRadius: '10px', // Add border radius to make it rounder
+    padding: '20px', // Add padding inside the border
+    display: 'inline-block' // Display the wheel container as inline-block to fit the content
   };
 
   return (
     <>
-      <p>HI</p>
-      <h1>Hello CodeSandbox</h1>
-      <div style={wheelContainerStyle}>
-        <WheelComponent
-          segments={segments}
-          segColors={segColors}
-          onFinished={(winner) => onFinished(winner)}
-          primaryColor="black"
-          contrastColor="white"
-          buttonText="Spin"
-          isOnlyOnce={false}
-          size={190}
-          upDuration={500}
-          downDuration={600}
-          fontFamily="Arial"
-        />
+      <div style={appStyle}>
+        <h1>Hello Aim, This is Your Lucky Draw</h1>
+        <div style={wheelContainerStyle}> {/* Apply wheel container style */}
+          <WheelComponent
+            segments={segments}
+            segColors={segColors}
+            onFinished={(winner) => onFinished(winner)}
+            primaryColor="black"
+            contrastColor="white"
+            buttonText="Spin2"
+            isOnlyOnce={false}
+            size={190}
+            upDuration={500}
+            downDuration={600}
+            fontFamily="Arial"
+          />
+        </div>
+        {/* <h2>Previous Spin Results:</h2>
+        <ul>
+          {previousResults.map((result, index) => (
+            <li key={index}>{result}</li>
+          ))}
+        </ul> */}
       </div>
-      <h2>Start editing to see some magic happen!</h2>
     </>
   )
 }
